3806163291d5a65517bb063009b2509d9849c062
Nakibul Hassan <nakibhasan2711@gmail.com>
2023-12-06 17:02:49 +0600

    - changed color values

--- app/src/main/java/com/alex/android/receipeappui/screens/DiscoverScreen.kt
+++ app/src/main/java/com/alex/android/receipeappui/screens/DiscoverScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -43,6 +44,7 @@ fun DiscoverScreen(navController: NavController) {
                     .background(MaterialTheme.colorScheme.background),
             ) {
                 RowWithCenteredTextAndIconButton(navController)
+                Spacer(modifier = Modifier.height(20.dp))
             }
 //            Column(
 //                modifier = Modifier.fillMaxSize().padding(15.dp),
