800a73831ca325acb2853737f413f0c6a85e82cb
Nakibul Hassan <nakibhasan2711@gmail.com>
2023-12-07 12:52:30 +0600

    - added cardview with image and title and a lazyrow

--- app/src/main/java/com/alex/android/receipeappui/screens/DiscoverScreen.kt
+++ app/src/main/java/com/alex/android/receipeappui/screens/DiscoverScreen.kt
@@ -1,16 +1,23 @@
 package com.alex.android.receipeappui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +26,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -29,14 +38,15 @@ import androidx.navigation.NavController
 import com.alex.android.receipeappui.R
 import com.alex.android.receipeappui.Screens
 import com.alex.android.receipeappui.ui.theme.ReceipeAppUITheme
+import com.alex.android.receipeappui.ui.theme.SmallCardBgcolor
 import com.alex.android.receipeappui.ui.theme.TopBarColor
+
 
 @Composable
 fun DiscoverScreen(navController: NavController) {
     ReceipeAppUITheme {
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = Color.Red
+            modifier = Modifier.fillMaxSize(), color = Color.Red
         ) {
             Column(
                 modifier = Modifier
@@ -45,24 +55,19 @@ fun DiscoverScreen(navController: NavController) {
             ) {
                 RowWithCenteredTextAndIconButton(navController)
                 Spacer(modifier = Modifier.height(20.dp))
+                LazyRow {
+                    items(1) {
+                        CardWithImageAndTitle(
+                            "American", painterResource(id = R.drawable.hamburger_)
+                        )
+                        CardWithImageAndTitle("Spanish", painterResource(id = R.drawable.food_))
+                        CardWithImageAndTitle("Italian", painterResource(id = R.drawable.pizza_))
+                        CardWithImageAndTitle("Chinese", painterResource(id = R.drawable.sushi_))
+                        CardWithImageAndTitle("Spanish", painterResource(id = R.drawable.food_))
+                    }
+                }
+
             }
-//            Column(
-//                modifier = Modifier.fillMaxSize().padding(15.dp),
-//                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.Center
-//            ) {
-//                Box(
-//                    modifier = Modifier.fillMaxSize().background(Color.Red)
-//                        .clip(MaterialTheme.shapes.large)
-//                ) {
-//
-//                }
-//                Text(
-//                    "Discover Screen",
-//                    style = MaterialTheme.typography.titleLarge,
-//                    modifier = Modifier.padding(vertical = 20.dp)
-//                )
-//            }
         }
     }
 }
@@ -89,14 +94,57 @@ fun RowWithCenteredTextAndIconButton(navController: NavController) {
         IconButton(
             onClick = {
                 navController.navigate(Screens.Settings.route)
-            },
-            modifier = Modifier
+            }, modifier = Modifier
                 .size(48.dp)
                 .padding(8.dp)
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.setting),
-                contentDescription = "Send"
+                painter = painterResource(id = R.drawable.setting), contentDescription = "Send"
+            )
+        }
+    }
+}
+
+@Composable
+fun CardWithImageAndTitle(
+    title: String, painter: Painter
+) {
+    Card(
+        modifier = Modifier
+            .height(150.dp)
+            .width(150.dp)
+            .padding(16.dp)
+            .clip(RoundedCornerShape(45.dp))
+            .background(MaterialTheme.colorScheme.onBackground),
+        colors = CardDefaults.cardColors(
+            containerColor = SmallCardBgcolor, //Card background color
+            contentColor = Color.Black  //Card content color,e.g.text
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(50.dp)
+                    .aspectRatio(1.2f),
+                alignment = Alignment.Center
+
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
             )
         }
     }
